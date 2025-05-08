e572963305d9484aa91ab3a25ec7745b807c2c96
소유나 <93272421+seacrab808@users.noreply.github.com>
2025-05-08 14:39:32 +0900

    Feat: 인터페이스 구현

--- src/com/gh/service/BookingService.java
+++ src/com/gh/service/BookingService.java
@@ -1,5 +1,31 @@
 package com.gh.service;
 
-public interface BookingService {
+import com.gh.model.Booking;
 
+/**
+ * 예약(Booking)에 대한 등록, 수정, 삭제 기능을 정의하는 서비스 인터페이스입니다.
+ * 
+ * <p>예약 데이터를 저장소(예: DB)에 연동하거나 관리하는 비즈니스 로직의 진입점 역할을 합니다.</p>
+ */
+public interface BookingService {
+	/**
+     * 새로운 예약 정보를 추가합니다.
+     *
+     * @param b 추가할 예약 객체 (Booking)
+     */
+	void addBooking(Booking b);
+	
+	 /**
+     * 예약 ID를 기반으로 기존 예약을 삭제합니다.
+     *
+     * @param bookingId 삭제할 예약의 고유 식별자
+     */
+	void deleteBooking(int bookingId);
+	
+	/**
+     * 기존 예약 정보를 수정합니다.
+     *
+     * @param b 수정할 예약 객체. bookingId를 포함해야 하며, 해당 ID의 예약이 존재해야 합니다.
+     */
+	void updateBooking(Booking b);
 }
