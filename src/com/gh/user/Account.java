302e60a935d1cfdeba69b20ff8ebe0ff4905fe6a
JunYong <128226527+azure0929@users.noreply.github.com>
2025-05-08 14:27:09 +0900

    Feat: Customer, Account 클래스 구현

--- src/com/gh/user/Account.java
+++ src/com/gh/user/Account.java
@@ -1,5 +1,51 @@
 package com.gh.user;
 
+/**
+ * Account 클래스는 사용자 계정 정보를 나타냅니다.
+ * 사용자 이름(username)과 잔액(balance)을 포함합니다.
+ * 
+ * @author YangJunYong
+ */
 public class Account {
+	private String username;  // 사용자 이름
+	private String balance;   // 계좌 잔액
 
+	/**
+	 * 기본 생성자입니다.
+	 * 객체를 초기값 없이 생성할 때 사용됩니다.
+	 */
+	public Account() {
+		super();
+	}
+
+	/**
+	 * 사용자 이름과 잔액을 초기화하는 생성자입니다.
+	 */
+	public Account(String username, String balance) {
+		super();
+		this.username = username;
+		this.balance = balance;
+	}
+
+	/**
+	 * 사용자 이름을 반환합니다.
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * 계좌 잔액을 반환합니다.
+	 */
+	public String getBalance() {
+		return balance;
+	}
+
+	/**
+	 * 객체의 문자열 표현을 반환합니다.
+	 */
+	@Override
+	public String toString() {
+		return "Account [username=" + username + ", balance=" + balance + "]";
+	}
 }
